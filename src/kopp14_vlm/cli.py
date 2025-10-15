9db1d10df8e0e88ee9f57acd90081d42eee4c911
Emma Marshall <emarshall@rhg.com>
2025-10-15 12:42:12 -0600

    wired up scripts + aux files

---
+++ src/kopp14_vlm/cli.py
@@ -0,0 +1,126 @@
+from kopp14_vlm.kopp14_verticallandmotion_preprocess import (
+    kopp14_preprocess_verticallandmotion,
+)
+from kopp14_vlm.kopp14_verticallandmotion_postprocess import (
+    kopp14_postprocess_verticallandmotion,
+)
+
+import click
+
+
+@click.command()
+@click.option(
+    "--pipeline-id", required=True, type=str, help="Unique identifier for the pipeline"
+)
+@click.option(
+    "--rate-file",
+    required=True,
+    type=str,
+    default="./data/input/bkgdrate-210306.tsv",
+    help="Path to the input rate file",
+)
+@click.option(
+    "--nsamps",
+    required=False,
+    type=int,
+    default=500,  # default in f1 script is 20k
+    help="Number of samples to generate",
+    show_default=True,
+)
+@click.option(
+    "--rng-seed",
+    required=False,
+    type=int,
+    default=1234,
+    help="Seed value for random number generator",
+    show_default=True,
+)
+@click.option(
+    "--baseyear",
+    required=False,
+    type=click.IntRange(min=2000, max=2300),
+    default=2000,
+    help="Base or referecne year for projections",
+    show_default=True,
+)
+@click.option(
+    "--pyear-start",
+    required=False,
+    type=click.IntRange(min=2000, max=2300),
+    default=2000,
+    help="Year for which projections start",
+    show_default=True,
+)
+@click.option(
+    "--pyear-end",
+    required=False,
+    type=int,
+    default=2100,
+    help="Year for which projections end",
+    show_default=True,
+)
+@click.option(
+    "--pyear-step",
+    required=False,
+    type=click.IntRange(min=1),
+    default=10,
+    help="Step size in years between pyear_start and pyear_end at which projections are produced",
+    show_default=True,
+)
+@click.option(
+    "--location-file",
+    required=True,
+    type=str,
+    default="./data/input/location.lst",
+    help="File that contains name, id, lat, and lon of points for localization",
+)
+@click.option(
+    "--chunk-size",
+    required=False,
+    type=int,
+    default=50,
+    help="Number of locations to process at a time",
+    show_default=True,
+)
+@click.option(
+    "--output_lslr_file",
+    required=False,
+    type=str,
+    default="./data/output/localsl.nc",
+    help="Path to the output local SLR netCDF file",
+)
+def main(
+    pipeline_id,
+    rate_file,
+    nsamps,
+    rng_seed,
+    baseyear,
+    pyear_start,
+    pyear_end,
+    pyear_step,
+    location_file,
+    chunk_size,
+    output_lslr_file,
+):
+    """ """
+    click.echo("Hello from Kopp14-vlm!")
+
+    preprocess_dict = kopp14_preprocess_verticallandmotion(pipeline_id, rate_file)
+    preprocess_message = (
+        f"Preprocess complete. Keys are: {list(preprocess_dict.keys())}"
+    )
+    click.echo(preprocess_message)
+
+    kopp14_postprocess_verticallandmotion(
+        preprocess_dict=preprocess_dict,
+        nsamps=nsamps,
+        rng_seed=rng_seed,
+        baseyear=baseyear,
+        pyear_start=pyear_start,
+        pyear_end=pyear_end,
+        pyear_step=pyear_step,
+        locationfile=location_file,
+        chunksize=chunk_size,
+        pipeline_id=pipeline_id,
+        output_lslr_file=output_lslr_file,
+    )
---
+++ src/kopp14_vlm/kopp14_verticallandmotion_preprocess.py
@@ -0,0 +1,79 @@
+import argparse
+
+""" kopp14_preprocess_verticallandmotion.py
+
+This runs the preprocessing stage for the vertical land motion component of the Kopp 14
+workflow.
+
+Parameters:
+pipeline_id = Unique identifier for the pipeline running this code
+baseyear = The year from which projections should be zeroed
+
+"""
+
+
+def kopp14_preprocess_verticallandmotion(pipeline_id, rate_file):
+    # Initialize variables to hold data and site information
+    names = []
+    ids = []
+    lats = []
+    lons = []
+    rates = []
+    sds = []
+
+    # Define the rate file name
+    # rate_file = os.path.join(os.path.dirname(__file__), "bkgdrate-210306.tsv")
+
+    # Open the rate file
+    with open(rate_file, "r") as f:
+        # Skip the first line (header info)
+        line = f.readline()
+
+        # Loop over the lines of the file
+        for line in f:
+            # Split the line into components
+            (this_name, this_id, this_lat, this_lon, this_rate, this_sd) = line.split(
+                "\t"
+            )
+
+            # Store the information
+            names.append(this_name)
+            ids.append(int(this_id))
+            lats.append(float(this_lat))
+            lons.append(float(this_lon))
+            rates.append(float(this_rate))
+            sds.append(float(this_sd))
+
+    # Populate the output dictionary
+    outdata = {
+        "names": names,
+        "ids": ids,
+        "lats": lats,
+        "lons": lons,
+        "rates": rates,
+        "sds": sds,
+    }
+
+    return outdata
+
+
+if __name__ == "__main__":
+    # Initialize the command-line argument parser
+    parser = argparse.ArgumentParser(
+        description="Run the pre-processing stage for the Kopp 14 vertical land motion workflow",
+        epilog="Note: This is meant to be run as part of the Kopp 14 module within the Framework for the Assessment of Changes To Sea-level (FACTS)",
+    )
+
+    # Define the command line arguments to be expected
+    parser.add_argument(
+        "--pipeline_id", help="Unique identifier for this instance of the module"
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Run the preprocessing stage with the user defined RCP scenario
+    kopp14_preprocess_verticallandmotion(args.pipeline_id)
+
+    # Done
+    exit()
